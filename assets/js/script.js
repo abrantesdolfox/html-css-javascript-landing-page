8827f75296aeee677d03a1ef8a7daa890cff41d7
Rodolfo Abrantes <rodolfo.abrantes@gmail.com>
2023-12-21 11:33:20 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+function imgSlider(anyThing) {
+    document.querySelector('.pepsi').src = anyThing
+}
+
+function changeBgColor(color){
+    const sec = document.querySelector('.sec')
+    sec.style.background = color
+}
